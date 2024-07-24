e2b835cf2f361b3e9baf106db70e4b63c9ac9554
Lorena Mesa <lorenanicole@github.com>
2024-07-24 21:55:06 +0000

    Begin implementing Python typing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import fastparquet
@@ -16,34 +16,22 @@ __email__ = "me@lorenamesa.com"
 class ExchangeRateAPI:
     API = 'https://api.fiscaldata.treasury.gov/services/api/'
     PROJECT_ROOT = Path(__file__).parent.absolute()
+    DATE_STR_FORMAT = '%Y-%m-%d'
     
     def __init__(self, since_date_str: str=None) -> None:
         self.since_date_str = self.__generate_quarterly_date_str__(since_date_str)
-        # import pdb; pdb.set_trace()
 
-    def get_quarter(self, p_date) -> int:
-        return (p_date.month - 1) // 3 + 1
+    def get_quarter(self, since_date) -> int:
+        return (since_date.month - 1) // 3 + 1
 
+    def get_first_day_of_the_quarter(self, since_date):
+        return datetime(since_date.year, 3 * ((since_date.month - 1) // 3) + 1, 1)
 
-    def get_first_day_of_the_quarter(self, p_date):
-        return datetime.datetime(p_date.year, 3 * ((p_date.month - 1) // 3) + 1, 1)
-
-
-    def get_last_day_of_the_quarter(self, p_date):
-        quarter = self.get_quarter(p_date)
-        return datetime.datetime(p_date.year + 3 * quarter // 12, 3 * quarter % 12 + 1, 1) + datetime.timedelta(days=-1)
+    def get_last_day_of_the_quarter(self, since_date):
+        quarter = self.get_quarter(since_date)
+        return datetime(since_date.year + 3 * quarter // 12, 3 * quarter % 12 + 1, 1) + timedelta(days=-1)
     
-    def __generate_quarterly_date_str__(self, since_date_str: str=None) -> str:
-        # if since_date_str:
-        #     year, month, day = since_date_str.split('-')
-        #     since_date = pd.Timestamp(int(year), int(month), int(day))
-        # else:
-        #     since_date = pd.Timestamp.today()
-
-        # # obtaing the quarter of the given year
-        # print("The given date can be found in quarter ", my_date.quarter)
-        
-        # date of previous quarter
+    def get_last_day_of_previous_quarter(self, since_date):
         # if since_date.month < 4:
         #     since_date = datetime.date(since_date.year - 1, 12, 31)
         # elif since_date.month < 7:
@@ -52,34 +40,47 @@ class ExchangeRateAPI:
         #     since_date = datetime.date(since_date.year, 6, 30)
         # else:
         #     since_date = datetime.date(since_date.year, 9, 30)
-        from datetime import datetime,timedelta
+        # return since_date
+        quarter = self.get_quarter(since_date)
+        return datetime(since_date.year, 3 * quarter - 2, 1) + timedelta(days=-1)
+    
+    def __is_date_in_future__(self, since_date_str: str) -> bool:
+        year, month, day = since_date_str.split('-')
+        since_date = datetime(int(year), int(month), int(day))
+        return since_date > datetime.now()
+    
+    def __generate_quarterly_date_str__(self, since_date_str: str=None) -> str:
+        """
+        Function that has to check the following conditions:
+        - If since_date_str provided is in the future, raise an error since no exchange data exists for the future
+        - If since_date_str happened in the past we need to capture get_last_day_of_the_quarter
+        to process the proper quartely data exchange reports.
+        - If since_date_str is not provided, default to using today's date so the quarter is still ongoing 
+        so we will want the get_last_day_of_the_previous_quarter.
+
+        :return: a datatime string in the format of ExchangeRateAPI.DATE_STR_FORMAT
+        """
+        if self.__is_date_in_future__(since_date_str):
+            raise ValueError(f'Date {since_date_str} provided is in the future, cannot retrieve exchange data.')
 
         if since_date_str:
+            year: str
+            month: str 
+            day: str
             year, month, day = since_date_str.split('-')
-            since_date = datetime(int(year), int(month), int(day))
+            since_date: datetime = datetime(int(year), int(month), int(day))
+            since_date: str = self.get_last_day_of_the_quarter(since_date.date())
         else:
-            since_date = datetime.now()
-            # TODO: If don't yet have current quarter data need to generate last quarter data
-
-        # from datetime import datetime,timedelta
-
-        # quarter = int((since_date.month - 1) / 3 + 1)
-        # last_day_previous_quarter = datetime(since_date.year, 3 * quarter - 2, 1) + timedelta(days=-1)
-
-        # import pdb; pdb.set_trace()
-
-        new_val = self.get_last_day_of_the_quarter(since_date.date())
-        new_val = new_val.strftime("%Y-%m-%d")
-        # import pdb; pdb.set_trace()
-        return new_val
-
+            since_date: datetime = datetime.now()
+            since_date: str = self.get_last_day_of_previous_quarter(since_date)
+        return since_date.strftime(ExchangeRateAPI.DATE_STR_FORMAT)
     
     def generate_parquet_quarterly_files(self):
         # Build query URL
         date_str = self.since_date_str
         parameter_str = f'?fields=country_currency_desc,exchange_rate,record_date&filter=record_date:gte:{date_str}'
         endpoint = f'{ExchangeRateAPI.API}/fiscal_service/v1/accounting/od/rates_of_exchange{parameter_str}'
-        # import pdb; pdb.set_trace()
+
         # Instantiate initial request into responses
         responses, response = [], requests.get(endpoint)
 
@@ -150,10 +151,10 @@ if __name__ == '__main__':
 
     def validate_date_str(date_str):
         try:
-            datetime.date.fromisoformat(date_str)
+            is_valid_date = bool(datetime.strptime(date_str, ExchangeRateAPI.DATE_STR_FORMAT))
             return date_str
         except ValueError:
-            raise argparse.ArgumentTypeError("Incorrect provided since date string provided, must be in format of YYYY-MM-DD.")
+            raise argparse.ArgumentTypeError("Invalid since date string provided, must be in format of YYYY-MM-DD.")
         
     parser.add_argument(
         "-s",
